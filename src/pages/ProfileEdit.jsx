9fe51610464fbf427c10beae7f5eb15a6f18477d
HiagoP <hiagoperonip@gmail.com>
2022-09-11 15:35:59 -0300

    Teste dos requisitos 1-7

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -2,11 +2,62 @@ import React from 'react';
 import Header from '../Components/Header';
 
 export default class ProfileEdit extends React.Component {
+  mudarEstado = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.verificarUser();
+    });
+  };
+
   render() {
     return (
       <div data-testid="page-profile-edit">
         <Header />
-        <p>Edit Profile</p>
+        <form>
+          <label htmlFor="nome">
+            Nome de Usuário:
+            <input
+              type="email"
+              name="nome"
+              id="nome"
+              data-testid="edit-input-name"
+              onChange={ this.mudarEstado }
+            />
+          </label>
+          <label htmlFor="email">
+            Email de Usuário:
+            <input
+              type="text"
+              name="email"
+              id="email"
+              data-testid="edit-input-email"
+              onChange={ this.mudarEstado }
+            />
+          </label>
+          <label htmlFor="description">
+            Descrição de Usuário:
+            <input
+              type="text"
+              name="description"
+              id="description"
+              data-testid="edit-input-description"
+              onChange={ this.mudarEstado }
+            />
+          </label>
+          <label htmlFor="imagem">
+            Imagem de Usuário:
+            <input
+              type="text"
+              name="imagem"
+              id="imagem"
+              data-testid="edit-input-image"
+              onChange={ this.mudarEstado }
+            />
+          </label>
+          <button type="button">Salvar</button>
+        </form>
       </div>
     );
   }
